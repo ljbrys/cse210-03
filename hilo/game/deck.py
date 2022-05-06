fc181e5a5be5cf88c3fe5f42820206ab743b970a
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-05-06 17:24:30 -0400

    Finished documentation, and testing uploads to help out Matt.

--- hilo/game/deck.py
+++ hilo/game/deck.py
@@ -14,10 +14,12 @@ class Deck:
     #     well as their location within the deck, as well as the cards that have already been used.
 
     # Attributes:
-    #     cards (list):
-    #     drawn_cards (list):
-    #     values (list):
-    #     suits (list):
+    #     cards (list):         A list to contain multiple card objects that are currently in the
+    #                               Deck
+    #     drawn_cards (list):   A list to contain multiple card objects that have been drawn from
+    #                               the deck
+    #     values (list):        A list of all face values present in this type of deck.
+    #     suits (list):         A list of all suit identities present in this type of deck.
     # """
     def __init__(self, values: list = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"], suits: list = ["Spades","Hearts","Clubs","Diamonds"]):
         # """Constructs a new instance of Deck
@@ -26,9 +28,9 @@ class Deck:
         #     this class is used for types of playing cards other than the traditional Western-52.
         
         # Args:
-        #     self (Deck): An instance of Deck.
-        #     values (list): A list of potential face values of cards.
-        #     suits (list): A list of potential suit identities of cards.
+        #     self (Deck):      An instance of Deck.
+        #     values (list):    A list of potential face values of cards.
+        #     suits (list):     A list of potential suit identities of cards.
         # """
         self.cards = []
         self.drawn_cards = []
@@ -42,14 +44,29 @@ class Deck:
         
 
     def shuffle(self):
+        # """Shuffles the cards that are currently in the deck
+        #
+        # Args:
+        #     self (Deck):      An instance of Deck.
+        # """
         random.shuffle(self.cards)
 
     def draw(self):
+        # """Draws a card from the deck and places it into the drawn_cards list
+        # 
+        # Args:
+        #     self (Deck):      An instance of Deck
+        # """
         drawn_card = self.cards.pop()
         self.drawn_cards.append(drawn_card)
         return drawn_card
 
     def fill(self):
+        # """Moves cards from the drawn_cards list back into the current deck
+        # 
+        # Args:
+        #     self (Deck):      An instance of Deck
+        # """
         for i in range(len(self.drawn_cards)):
             self.cards.append(self.drawn_cards.pop())
 
