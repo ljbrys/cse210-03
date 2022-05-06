45fa295415c06d29cd44af9fee12f50c0f6af726
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-05-05 23:54:02 -0400

    Dylan's initial commit

---
+++ hilo/game/deck.py
@@ -0,0 +1,22 @@
+from game.card import Card
+import random
+
+class Deck:
+    def __init__(self, values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"], suits = ["Spades","Hearts","Clubs","Diamonds"]):
+        self.cards = []        
+        self.paramaters = [values = values, suits = suits]
+        self.fill()
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw(self):
+        return self.cards.pop()
+
+    def fill(self):        
+        for i in range(len(self.paramaters.suits)):
+            for j in range(len(self.paramaters.values)):
+                self.cards.append(Card(self.paramaters.values[j],self.paramaters.suits[i]))
+
+    def deck_check(self):
+        print(self.cards)
