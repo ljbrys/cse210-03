479df042c2111008e24515f75aecbffeb11232bb
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-05-11 16:02:43 -0400

    got rid of the redundant functions, removed some whitespace, and working on calculate_score

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -39,27 +39,6 @@ class Director:
             self.get_next_card()
             self.calculate_score()
             self.play_again()
-
-
-    def get_inputs(self):
-        """Ask the user if they want to pick another card.
-        
-        Args:
-            self (Director): An instance of Director.
-        """
-        get_card = input("Pick a card? [y/n] ")
-        self.continue_playing = (get_card == "y")
-
-    def do_updates(self):
-        """Updates the player's score.
-        
-        Args:
-            self (Director): An instance of Director.
-        """
-        if not self.continue_playing:
-            return
-
-        #for i in range(len(self.))
     
     def display_card(self):
         # this is a test Larry J. Brys
@@ -73,10 +52,10 @@ class Director:
         pass
 
     def calculate_score(self):
-        # self.score
+        # self.
+        
         pass
 
     def play_again():
         pass
         #I'll take a shot at this one - Matt
-
